d755a1237283d2550391bdff0f718548f34c3f53
Ioann Mayboroda <133093225+iomayb@users.noreply.github.com>
2025-11-24 22:14:51 +0300

    Fix faucet

--- modules/neuraverse.py
+++ modules/neuraverse.py
@@ -215,7 +215,7 @@ class NeuraVerse:
 
             headers = {"Referer": "https://neuraverse.neuraprotocol.io/?section=faucet"}
 
-            response = await self.session.get(url="https://neuraverse.neuraprotocol.io/_next/static/chunks/9830-de9a01275985caba.js", headers=headers)
+            response = await self.session.get(url="https://neuraverse.neuraprotocol.io/_next/static/chunks/3160-c1da923f868648d0.js", headers=headers)
             logger.debug(f"{self.wallet} | Faucet JS chunk status: {response.status_code}")
 
             if response.status_code != 200:
